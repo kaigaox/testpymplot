3c7ef5097fa7d9da217adef412d932caf537f941
Kai Gao <nebulaekg@gmail.com>
2021-09-13 15:21:55 -0600

    test

--- c.sh
+++ c.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --volume /home/kaigao/Desktop/test:/data --user $(id -u):$(id -g) --env JOURNAL=joss openjournals/paperdraft
+docker run --rm --volume /home/kaigao/Desktop/test:/data --user $(id -u):$(id -g) --env JOURNAL=joss openjournals/paperdraft
